f8443de9aea9c796b3f30608c2c69aca2e616a1d
Bumblebee Bee <bumbelbee437167@gmail.com>
2026-04-14 20:06:51 +0100

    [CI/fix] i'm tired, boss

--- source/Core/Debug/Debug.hxx
+++ source/Core/Debug/Debug.hxx
@@ -2,6 +2,7 @@
 
 #include <sstream>
 #include <string>
+#include <utility>
 
 namespace Solstice::Core {
 
@@ -11,6 +12,14 @@ public:
     static void LogMessage(const std::string& message);
 };
 
+/** Streams any number of arguments (like std::cout) into the debug log. */
+template<typename... Args>
+inline void SolsticeLog(Args&&... args) {
+    std::ostringstream os;
+    (os << ... << std::forward<Args>(args));
+    DebugLogger::LogMessage(os.str());
+}
+
 } // namespace Solstice::Core
 
 #define SIMPLE_LOG(msg)                                                                                \
@@ -19,3 +28,5 @@ public:
         _solstice_simple_log_ss_ << (msg);                                                             \
         ::Solstice::Core::DebugLogger::LogMessage(_solstice_simple_log_ss_.str());                     \
     } while (0)
+
+#define SOLSTICE_LOG(...) ::Solstice::Core::SolsticeLog(__VA_ARGS__)
